2b38b5f62f7e41a05bac14a91c358f72c874aff6
Linus Miller <lohfu@lohfu.io>
2015-10-22 20:25:38 +0200

    Poirot.

--- filters/poirot.js
+++ filters/poirot.js
@@ -1,43 +1,31 @@
-// lower case
-function lc(value, context) {
-	return value.toLowerCase();
+var poirot = require('poirot');
+
+function lowerCase(value, context) {
+	return typeof value === 'string' ? value.toLowerCase() : null;
 }
 
-// lower case
-function uc(value) {
-	return value.toUpperCase();
-}
-
-// pascal case
-function pc(value) {
-	return typeof value === 'string' ? value.toPascalCase() : null;
-}
-
-// camelcase
-function cc(value) {
-	return typeof value === 'string' ? value.toCamelCase() : null;
-}
-
-// spinalcase
-function sc(value) {
-	return typeof value === 'string' ? value.toSpinalCase() : null;
-}
-
-// space case
-function Sc(value) {
-	return typeof value === 'string' ? value.toSpaceCase() : null;
-}
-
-function SC(value) {
-	return typeof value === 'string' ? value.toSpaceCase(true) : null;
+function upperCase(value) {
+	return typeof value === 'string' ? value.toUpperCase() : null;
 }
 
 module.exports = {
-	cc: cc,
-	lc: lc,
-	pc: pc,
-	sc: sc,
-	Sc: Sc,
-	SC: SC,
-	uc: uc
+	camelCase: poirot.camelCase,
+	cc: poirot.camelCase,
+	pascalCase: poirot.pascalCase,
+	pc: poirot.pascalCase,
+
+	kebabCase: poirot.spinalCase,
+	kc: poirot.spinalCase,
+	spinalCase: poirot.spinalCase,
+	sc: poirot.spinalCase,
+
+	lowerCase: lowerCase,
+	lc: lowerCase,
+	upperCase: upperCase,
+	uc: upperCase,
+
+	spaceCase: poirot.spaceCase,
+	Sc: poirot.spaceCase,
+	capitalSpace: poirot.capitalSpaceCase,
+	cSc: poirot.capitalSpaceCase
 };
