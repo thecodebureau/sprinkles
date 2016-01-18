7acc518987bcdfadf71364e32aa3e7511a12ef47
Linus Miller <lohfu@lohfu.io>
2016-01-18 14:17:37 +0100

    Renamed poirot.js to string-case.js and started using lodash string functions isntead of poirot.

---
+++ filters/string-case.js
@@ -0,0 +1,26 @@
+function pascalCase(value) {
+	return _.upperFirst(_.camelCase(value));
+}
+
+module.exports = {
+	camelCase: _.camelCase,
+	cc: _.camelCase,
+
+	pascalCase: pascalCase,
+	pc: pascalCase,
+
+	kebabCase: _.kebabCase,
+	kc: _.kebabCase,
+
+	lowerCase: _.lowerCase,
+	lc: _.lowerCase,
+
+	upperCase: _.upperCase,
+	uc: _.upperCase,
+
+	snakeCase: _.snakeCase,
+	sc: _.snakeCase,
+
+	startsCase: _.startsCase,
+	Sc: _.startsCase,
+};
