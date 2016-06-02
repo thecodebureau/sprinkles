4438df0c608ee4205899106135789107b4d69b6c
Linus Miller <lohfu@lohfu.io>
2016-06-02 12:37:08 +0200

    Only en locale by default. Use easy-tz to add other locales.

--- util/locales.js
+++ util/locales.js
@@ -1,10 +1,8 @@
+'use strict';
+
 module.exports = {
-	'sv': {
-		months: [ 'januari', 'febuari', 'mars', 'april', 'maj', 'juni', 'juli', 'augusti', 'september', 'oktober', 'november', 'december' ],
-		days: [ 'söndag',  'måndag', 'tisdag', 'onsdag', 'torsdag', 'fredag', 'lördag' ]
-	},
-	'en': {
-		months: [ 'January', 'Febuary', 'Mars', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
-		days: [ 'Sunday',  'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
-	}
+  en: {
+    months: [ 'January', 'Febuary', 'Mars', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
+    days: [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
+  }
 };
